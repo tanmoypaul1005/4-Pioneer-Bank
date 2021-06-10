5ab1b1c08e7b3f5d59bbf81f70682e289cb3e12d
tanmoypaul1005 <84624966+tanmoypaul1005@users.noreply.github.com>
2021-06-10 21:01:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+//  Login Button even Handler   
+document.getElementById('login').addEventListener('click', function(){
+    document.getElementById('login-area').style.display='none';
+    document.getElementById('transaction-area').style.display="block";
+   
+    })
+    //diposit Button even Handler
+    document.getElementById('addDeposit').addEventListener('click', function(){
+    const depositNumber=getInputNumber("depositAmount");
+   
+    updateSpanText("currentDeposit",depositNumber);
+    updateSpanText("currentBalance",depositNumber);
+    document.getElementById('depositAmount').value="";
+   })
+   
+   
+   //withdraw Button even Handler
+   document.getElementById('addWithdraw').addEventListener('click',function(){
+     const withdrawNumber=getInputNumber("withdrawAmount");
+   
+     updateSpanText("currentWithdraw",withdrawNumber);
+     updateSpanText("currentBalance",-1* withdrawNumber)
+     document.getElementById('withdrawAmount').value="";
+   })
+   
+   
+   
+   
+   
+   
+   function getInputNumber(id){
+   const Amount=document.getElementById(id).value; 
+   const AmountNumber=parseFloat(Amount); 
+   return AmountNumber;
+   }
+    
+    function updateSpanText(id ,depositNumber){
+    const current=document.getElementById(id).innerText;
+    const currentNumber=parseFloat(current);
+    const totalAmount=depositNumber + currentNumber;
+    document.getElementById(id).innerText=totalAmount;
+    }
+    
